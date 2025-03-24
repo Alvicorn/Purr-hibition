38be06b401a7118bec9bee531671388b36624fdc
Alvin Tsang <act18@sfu.ca>
2025-03-24 16:25:00 -0700

    add motion sensor and cat detection to main - running seperately

--- app/src/main.c
+++ app/src/main.c
@@ -19,6 +19,12 @@ void* detect_a_cat(void* arg) {
   }
 }
 
+void* motion_sensor_thread_func(void* arg) {
+  (void)arg;
+  MotionSensor_doState();
+  return NULL;
+}
+
 int main() {
   printf("Starting Purr-hibition!\n");
 
@@ -32,14 +38,16 @@ int main() {
   pthread_t motion_sensor_thread;
 
   pthread_create(&cat_detection_thread, NULL, detect_a_cat, NULL);
+  pthread_create(&motion_sensor_thread, NULL, motion_sensor_thread_func, NULL);
 
   while (true) {
   }
 
   pthread_cancel(cat_detection_thread);
-  pthread_join(cat_detection_thread, NULL);
+  pthread_cancel(motion_sensor_thread);
 
-  MotionSensor_doState();
+  pthread_join(cat_detection_thread, NULL);
+  pthread_join(motion_sensor_thread, NULL);
 
   // clean up resources
   CatDetector_cleanup();
