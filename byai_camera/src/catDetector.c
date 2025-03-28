5d588f05a608fb1cf4ac6c321a6d227ed22084d0
Alvin Tsang <act18@sfu.ca>
2025-03-28 02:01:53 -0700

    update CatDetector.* to use hal/sharedMemory.*

---
+++ byai_camera/src/catDetector.c
@@ -0,0 +1,33 @@
+#include "byai_camera/catDetector.h"
+
+#include <assert.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+#include "hal/sharedMemory.h"
+
+#define SHARED_MEM_NAME "/byai_cam_cat_detected"  // Shared memory object name
+
+static bool is_initialized = false;
+static SharedMemory_t shm;
+
+void CatDetector_init(void) {
+  assert(!is_initialized);
+  shm = SharedMemory_init(SHARED_MEM_NAME);
+  is_initialized = true;
+}  // end of CatDetector_init()
+
+void CatDetector_cleanup(void) {
+  assert(is_initialized);
+  SharedMemory_cleanup(&shm);
+  is_initialized = false;
+}  // end of CatDetector_cleanup()
+
+bool CatDetector_detected(void) {
+  return SharedMemory_read(&shm) == 1;  // 1 for cat detected
+}  // end of CatDetector_detected()
